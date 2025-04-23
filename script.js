e09637bdb17c32c0304ccbda9e27f4993319f475
Noam Shemer <noam@Noams-MacBook-Pro.local>
2025-04-23 13:28:52 +0300

    Added styles and js for toggling sections

---
+++ script.js
@@ -0,0 +1,8 @@
+function onClickPrograming(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
