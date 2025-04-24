6a68b085ae2e74d893052437424a540e16f4d189
Noam Shemer <noam@Noams-MacBook-Pro.local>
2025-04-24 20:21:45 +0300

    Added contact section

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+var contactOpen = false;
+const contactDeltaHeight = 160;
+
 function onClickHeader(id) {
     var element = document.getElementById(id);
+    if (element.style.display === "block") {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
+
+function onClickContact() {
+    var element = document.getElementById('footer');
+    if (contactOpen) {
+        element.style.height = (element.offsetHeight - contactDeltaHeight) + "px";
+    } else {
+        element.style.height = (element.offsetHeight + contactDeltaHeight) + "px";
+    }
+    contactOpen = !contactOpen;
+    var element = document.getElementById('contact');
     if (element.style.display === "block") {
         element.style.display = "none";
     } else {
